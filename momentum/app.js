4262c6488c653dd12553c77eab15e81fb63c0c10
JJ <ghks9705@gmail.com>
2026-06-22 23:35:39 +0900

    뭐라는지 하나도 모르겠농,,,,,

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,8 @@
-alert("hi");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+
+function onLoginBtnClick() {
+    console.log("Hello", userName);
+}
+
+loginButton.addEventListener("click", onLoginBtnClick);
