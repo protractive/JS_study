80b0b156828bfbdf7c13f7a606829be26b099557
JJ <ghks9705@gmail.com>
2026-06-23 22:13:29 +0900

    3-5 까지 진행

--- momentum/app.js
+++ momentum/app.js
@@ -1,8 +1,16 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
 
-function onLoginBtnClick() {
-    console.log("Hello", userName);
+const HIDDEN_CLASSNAME = "hidden"; // string만 담고 있는 변수는 대문자로 작성하는 것이 관례
+
+function onLoginSubmit(event) {
+    event.preventDefault(); // 브라우저의 기본 동작을 막아줌 예를 들어 새로고침이 일어나지 않도록 막아줌
+    loginForm.classList.add(HIDDEN_CLASSNAME); // form 숨기기
+    const username = loginInput.value; // input value 가져오기
+    localStorage.setItem("username", username); // localStorage에 username 저장
+    greeting.innerText = `Hello ${username}`; // greeting에 input value 넣기
+    greeting.classList.remove(HIDDEN_CLASSNAME); // greeting 보이기
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit); // submit 이벤트가 발생하면 onLoginSubmit 함수 실행
